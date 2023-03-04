ec9cbe44185219c5fe7bc37be20b2e540595cb96
soniaa28 <sofija.demchuk@gmail.com>
2023-03-04 22:22:47 +0200

    lab_2

---
+++ LAB_2/NumberProgression.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2
+{
+     public class NumberProgression
+    {
+        private double m1;// початковий член
+        private double increment;
+        protected static int n =10 ;
+
+        public NumberProgression()
+        {
+            m1=1 ;
+            increment = 1;
+            
+          
+        }
+        public NumberProgression(double _m1 , double i = 1)
+        {
+            m1 = _m1;
+            increment = i; 
+           
+        }
+
+        //property 
+        public double FirstMember
+        {
+            get
+            {
+                return m1;
+            }
+            set
+            {
+                m1 = value;
+            }
+        }
+
+        public static void setN(int value)
+        {
+            n = value;
+        }
+        public override string ToString()
+
+        {
+            string progression = "";
+            for (int i = 0; i < n; i++)
+            {
+                progression += string.Format("{0} ", this.getN(i));
+            }
+            return progression;
+        }
+
+        public double getN(int _n )
+        {
+           if (_n == 0) { return m1; }
+           else
+            {
+                return m1 + _n; //оскільки для базового класу використовуємо інкремент 1, для арифметичної і геометричної прогресії перевизначу 
+            }
+        }
+
+        public double getSumOfN(int _n )
+        {
+            double sum = 0;
+            for (int i = 0; i < _n; i++)
+            {
+                sum += this.getN(i);
+            }
+            return sum;
+        }
+        public double getSumOfAll()
+        {
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += this.getN(i);
+            }
+            return sum;
+        }
+        public string getProgression()
+        {
+            string progression = "";
+            for (int i = 0; i < n; i++)
+            {
+                progression += string.Format("{0} ", this.getN(i));
+            }
+            return progression;
+        }
+        public static bool operator <(NumberProgression left, NumberProgression right)
+        {
+            return left.getSumOfAll() < right.getSumOfAll();
+        }
+
+        public static bool operator >(NumberProgression left, NumberProgression right)
+        {
+            return left.getSumOfAll() > right.getSumOfAll();
+        }
+
+        public static NumberProgression operator +(NumberProgression left, NumberProgression right)
+        {
+            double newFirstMember = left.m1 + right.m1;
+            double newIncrement = 1;//напркилад згодом для арифметичної можна зробити так : left.increment + right.increment , а для геометричної множити 
+            return new NumberProgression(newFirstMember, newIncrement);
+        }
+    }
+
+    }
+
--- LAB_2/Program.cs
+++ LAB_2/Program.cs
@@ -1,1 +1,91 @@
-﻿
+﻿using System.Numerics;
+
+namespace LAB_2;
+class Program
+{
+
+    
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        NumberProgression p1 = new NumberProgression(2);
+        NumberProgression p2 = new NumberProgression(4);
+        NumberProgression p3 = p1 + p2;
+        NumberProgression p4 = new NumberProgression();
+        p4.FirstMember = 5;
+        NumberProgression p5 = p1 + p4;
+
+
+        Console.WriteLine(p1.getSumOfN(5));
+
+        Console.WriteLine(p1.getSumOfAll());
+        Console.WriteLine(p1.getProgression());
+        Console.WriteLine(p3.ToString());
+        Console.WriteLine(p2 > p1);
+
+        List<NumberProgression> set = new List<NumberProgression>() { p1, p2, p3, p4, p5 };
+
+
+        for (int i = 0; i < set.Count; i++)
+        {
+            Console.WriteLine(string.Format("Progression №{0} : {1} ", i + 1, set[i].ToString()));
+        }
+        void Menu()
+        {
+            bool _continue = true;
+            while (_continue)
+            {
+                Console.WriteLine("Оберіть один з варіантів :");
+                Console.WriteLine("1.Додати нову прогресію.");
+                Console.WriteLine("2.Надрукувати всі прогресії.");
+                Console.WriteLine("3.Знайти найбільшу прогресію(за сумою чисел)");
+                Console.WriteLine("4.Вийти");
+
+                string menu = Console.ReadLine();
+
+                switch (menu)
+                {
+                    case "1":
+                        Console.WriteLine("Щоб додати нову прогресію введіть перший член та інкремент через пробіл");
+
+                        string[] progression = Console.ReadLine().Split(" ");
+                        NumberProgression newProgression = new NumberProgression(double.Parse(progression[0]), double.Parse(progression[1]));
+                        set.Add((NumberProgression)newProgression);
+                        Console.WriteLine("Прогресія успішно додана до колекції.");
+                        break;
+                    case "2":
+                        for (int i = 0; i < set.Count; i++)
+                        {
+                            Console.WriteLine(string.Format("Progression №{0} : {1} ", i + 1, set[i].ToString()));
+                        }
+                        break;
+                    case "3":
+                        double max_sum = 0;
+                        int index = 0;
+                        for(int i = 0; i < set.Count; i++)
+                        {
+                            if (set[i].getSumOfAll()>max_sum)
+                            {
+                                max_sum = set[i].getSumOfAll();
+                                index = i;
+                            }
+                        }
+                        Console.WriteLine(String.Format("Прогресія {0} має найбільшу суму елементів - {1}", index + 1, set[index]));
+
+                        break;
+                    case "4":
+                    default:
+                        _continue = false;
+                        break;
+
+                       
+                }
+            }
+
+
+
+
+        }
+        Menu();
+    }
+}
